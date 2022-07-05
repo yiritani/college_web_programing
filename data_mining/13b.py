5056c2620eb91af52863a11e63bb10a68754391f
yiritani <74131902+yiritani@users.noreply.github.com>
2022-07-06 00:03:04 +0900

    aaa

--- data_mining/13b.py
+++ data_mining/13b.py
@@ -1,24 +1,22 @@
 import csv
 import glob
 import itertools
+
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 stop_word_list = [i[0] for i in csv.reader(open('stop_word.csv', 'r'))]
 vectorizer = TfidfVectorizer()
 
 
-class WordVector:
-	def __init__(self):
-		self.generate_word_vector()
-
-	@classmethod
-	def read_all_documents(cls):
+class Util:
+	@staticmethod
+	def get_all_documents():
 		file_name_list = glob.glob('21Documents/*.txt')
 		file_name_list.sort()
 		return file_name_list
 
-	@classmethod
-	def read_21documents(cls, file_name):
+	@staticmethod
+	def read_21documents(file_name) -> str:
 		word_list, tmp = [], []
 		with open(file_name, 'r') as f:
 			text = f.read()
@@ -27,12 +25,16 @@ class WordVector:
 			if word not in stop_word_list:
 				word_list.append(word)
 		result_text = ' '.join(word_list)
-		print(f"Text data : {result_text}")
 		return result_text
+
+
+class WordVector(Util):
+	def __init__(self):
+		self.generate_word_vector()
 
 	@classmethod
 	def generate_word_vector(cls):
-		for file_name in cls.read_all_documents():
+		for file_name in cls.get_all_documents():
 			print('=' * 50)
 			print(f' File name = {file_name}')
 			print('=' * 50)
@@ -45,5 +47,27 @@ class WordVector:
 			print('\n')
 
 
+class IDF(Util):
+	def __init__(self):
+		self.calc_idf()
+
+	@classmethod
+	def calc_idf(cls):
+		idf_dict = {}
+		document_list = cls.get_all_documents()
+		for file_name in document_list:
+			text = cls.read_21documents(file_name)
+			text_corpus = text.split('\n')
+			X = vectorizer.fit_transform(text_corpus)
+			for word in vectorizer.get_feature_names():
+				if word not in idf_dict:
+					idf_dict[word] = 0
+				idf_dict[word] += 1
+		for key, value in idf_dict.items():
+			idf_dict[key] = round(len(document_list) / value, 2)
+		print(idf_dict)
+
+
 if __name__ == '__main__':
 	WordVector()
+	IDF()
