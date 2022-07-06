fe49554c9bcc2cd07a2a8b2ae9c8c5ff94a67b16
yiritani <74131902+yiritani@users.noreply.github.com>
2022-07-07 00:05:41 +0900

    aaa

--- data_mining/13b.py
+++ data_mining/13b.py
@@ -2,12 +2,15 @@
 M21W0B09 入谷雄介
 Data mining 13 class task
 """
-
 import csv
 import glob
 import itertools
+import math
+import numpy as np
 
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.cluster import KMeans
 
 stop_word_list = [i[0] for i in csv.reader(open('stop_word.csv', 'r'))]
 vectorizer = TfidfVectorizer()
@@ -98,40 +101,91 @@ class FreqDamp(IdfUtil):
 
 class NormalizedFreq(IdfUtil):
 	@classmethod
-	def calc_normalized_freq(cls):
-		normalized_freq_dict = {}
-		document_list = cls.get_all_documents()
-		for file_name in document_list:
-			text = cls.read_21documents(file_name)
-			text_corpus = text.split('\n')
-			vectorizer.fit_transform(text_corpus)
-			for word in vectorizer.get_feature_names():
-				if word not in normalized_freq_dict:
-					normalized_freq_dict[word] = 0
-				normalized_freq_dict[word] += 1
-		for key, value in normalized_freq_dict.items():
-			normalized_freq_dict[key] = round(value / len(document_list), 2)
-		print('=' * 30, 'Normalized Frequency', '=' * 30)
-		print(normalized_freq_dict)
+	def calc_normalized_freq(cls, file_name):
+		idf_dict = {}
+		text = cls.read_21documents(file_name)
+		unique_words = text.split(' ')
+		text_corpus = text.split('\n')
+		N = len(text_corpus)
+		for unique_word in unique_words:
+			count = 0
+			for sen in text_corpus:
+				if unique_word in sen.split():
+					count = count + 1
+				idf_dict[unique_word] = (math.log((1 + N) / (count + 1))) + 1
+		return idf_dict
 
 
 class CosineSimilarity(IdfUtil):
 	@classmethod
-	def calc_cosine_similarity(cls):
-		document_list = cls.get_all_documents()
-		for file_name in document_list:
-			text = cls.read_21documents(file_name)
-			text_corpus = text.split('\n')
-			vectorizer.fit_transform(text_corpus)
-			X = vectorizer.transform(text_corpus)
-			print(X.toarray())
-			print('\n')
+	def calc_cosine_similarity(cls, doc_pair):
+		docs = [cls.read_21documents(doc_pair[0]), cls.read_21documents(doc_pair[1])]
+		vectorizer = TfidfVectorizer(max_df=0.9)
+		X = vectorizer.fit_transform(docs)
+		print('feature_names:', vectorizer.get_feature_names())
+		print('X:')
+		print(X.toarray())
+		sim = cosine_similarity(X)  # 類似度行列の作成
+		for from_id in range(len(docs)):
+			print('doc_id:', from_id)
+			for to_id in range(len(docs)):
+				print('\tsim[{0}][{1}] = {2:f}'.format(
+					from_id, to_id, sim[from_id][to_id]))
+
+
+class Jaccard(IdfUtil):
+	@classmethod
+	def jaccard(cls, doc1, doc2):
+		data1 = cls.read_21documents(doc1).split(' ')
+		data2 = cls.read_21documents(doc2).split(' ')
+		items = 0
+		for item in data1:
+			if item in data2:
+				items += 1
+		print('=' * 50)
+		print(doc1, doc2)
+		print(items / (len(data1) + len(data2) - items))
+
+
+class Kmeans(IdfUtil):
+	def __init__(self, cluster_num):
+		self.docs = self.get_all_documents()
+		self.doc_num = len(self.docs)
+		self.cluster_num = cluster_num
+
+	@classmethod
+	def separate_clusters(cls, input_docs):
+		docs = np.array([
+			cls.read_21documents(**input_docs)
+		])
+		vectorizer = TfidfVectorizer(use_idf=True, token_pattern=u'(?u)\\b\\w+\\b')
+		vecs = vectorizer.fit_transform(docs)
+
+		clusters = KMeans(n_clusters=2, random_state=0).fit_predict(vecs)
+		for doc, classes in zip(docs, clusters):
+			print(classes, doc)
 
 
 if __name__ == '__main__':
+	# (1)
 	# WordVector()
+	# (2)
 	# IDF()
-	FreqDamp()
-	print(NormalizedFreq())
-	CosineSimilarity.calc_cosine_similarity()
+	# (3)
+	# FreqDamp()
 
+	target_doc1 = ('21Documents/DOC01.txt', '21Documents/DOC02.txt')
+	target_doc2 = ('21Documents/DOC01.txt', '21Documents/DOC15.txt')
+	target_doc3 = ('21Documents/DOC20.txt', '21Documents/DOC21.txt')
+
+	# (4)
+	# for target_doc in [target_doc1, target_doc2, target_doc3]:
+	# 	CosineSimilarity.calc_cosine_similarity(target_doc1)
+
+	# (5)
+	# print('Jaccard')
+	# for target_doc in [target_doc1, target_doc2, target_doc3]:
+	# 	Jaccard.jaccard(target_doc[0], target_doc[1])
+
+	# (6)
+	Kmeans.separate_clusters(['21Documents/DOC03.txt', '21Documents/DOC07.txt', '21Documents/DOC11.txt'])
